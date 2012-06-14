7080f3abc51d9319445a74e15a7da322debc23c2
Ryan DeBeasi <rdebeasi@352media.com>
2012-06-14 15:33:59 -0400

    Added demo page.

---
+++ README.md
@@ -0,0 +1,74 @@
+#flexMenu
+flexMenu is a jQuery plugin that lets you create responsive menus that automatically collapse into a "more" drop-down when they run out of space.
+
+Demo! (Coming soon)
+
+[Source on GitHub](https://github.com/352Media/flexMenu)
+
+Written by [Ryan DeBeasi](http://www.ryandebeasi.com/) of [352 Media Group](http://www.352media.com/).
+
+##Usage
+
+Create an unordered list that contains your menu items. In CSS, use `display: inline-block;` or `float: left;` to get the  `li` elements to line up horizontally.
+
+Call flexMenu on an unordered list that contains your menu items.
+
+```javascript
+$('ul.menu.flex').flexMenu();
+```
+
+If you are using this script on a responsive site, you'll also want to recalculate the menus on resize. Because many resize events will be fired as the user resizes the window, it's best to wait a few hundred milliseconds after the last resize event fires before recalculating the menu.
+
+```javascript
+$(window).resize(function () {
+	resizeTimeout = setTimeout(resizeFunctions, 300);
+});
+
+function resizeFunctions() {
+	$('ul.menu.flex').flexMenu({'undo': true });
+	$('ul.menu.flex').flexMenu();
+}
+```
+
+In most cases, you'll also want to set the popup menu to be absolutely positioned:
+```css
+.flexMenu-popup {
+  position: absolute;
+}
+```
+
+##Dependencies
+
+###jQuery
+I've tested the plugin in jQuery 1.7.1 and 1.7.2.
+
+###Modernizr
+[Modernizr](http://modernizr.com/) is optional. If it's available, flexMenu will use it to detect touch support. If touch support is available, we'll toggle the menu on click. If touch support is not available, we'll toggle the menu on hover in/out. That way, we can avoid triggering the funky [simulated mouseover/mouseout](http://developer.apple.com/library/ios/#DOCUMENTATION/AppleApplications/Reference/SafariWebContent/HandlingEvents/HandlingEvents.html#//apple_ref/doc/uid/TP40006511-SW17) that happens on many touchscreen devices.
+
+If Modernizr is not available, we'll always toggle the menu on click.
+
+The zip for flexMenu includes a build of Modernizr contains only touch detection and the has-js/no-js class. Feel free to use this build, or go with more full-featured build if you're using other features. Or, if you do want to always toggle the menu on click, there is no need to include Modernizr at all.
+
+##Advanced usage
+
+If you're feeling fancy, you can include any of the following options when calling flexMenu:
+
+###threshold
+(integer, defaults to 2)
+If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
+
+###linkText
+(string, defaults to 'More')
+What text should we display on the "view  more" link?
+
+###linkTitle'
+(string, defaults to 'View More')
+What should the title of the "view more" button be?
+
+###showOnHover
+(boolean, defaults to 'true')
+Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
+
+###undo
+(boolean, defaults to 'false')
+If this is true, we'll move the list items back to where they were before, and remove the "View More" link. This is useful if you actually _do_ want list items to stack in some cases, or if you want to recalculate the menu.
---
+++ flexmenu.js
@@ -0,0 +1,80 @@
+/*	jQuery.flexMenu
+	Author: Ryan DeBeasi (352 Media Group) - http://www.352media.com/
+	Description: If a list is too long for all items to fit on one line, display a popup menu instead. 
+	Dependencies: jQuery, Modernizr. Modernizr is optional and allows us to force requireClick to be true if touch is supported. */
+(function ($) {
+	$.fn.flexMenu = function (options) {
+		var s = $.extend({
+			'threshold'	: 2,					// [integer] If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
+			'linkText' : 'More',				// [string] What text should we display on the "view  more" link?
+			'linkTitle' : 'View More',			// [string] What should the title of the "view more" button be?
+			'showOnHover' : true,				// [boolean] Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
+			'undo' : false						// [boolean] Move the list items back to where they were before, and remove the "View More" link.
+		}, options);
+		return this.each(function () {
+			var $this = $(this),
+				$firstItem = $this.find('li:first-child'),
+				$lastItem = $this.find('li:last-child'),
+				numItems = $this.find('li').length,
+				firstItemTop = Math.floor($firstItem.offset().top),
+				firstItemHeight = Math.floor($firstItem.height()),
+				$lastChild,
+				keepLooking,
+				$moreItem,
+				$moreLink,
+				numToRemove,
+				$menu,
+				i;
+			function needsMenu($itemOfInterest) {
+				var result = (Math.ceil($itemOfInterest.offset().top) >= (firstItemTop + firstItemHeight)) ? true : false;
+				return result;
+			}
+			if (needsMenu($lastItem) && numItems > s.threshold && !s.undo && $this.is(':visible')) {
+			// Values may be calculated from em and give us something other than round numbers. Browsers may round these inconsistently. So, let's round numbers to make it easier to trigger flexMenu.
+				var $popup = $('<ul class="flexMenu-popup" style="display:none;"></ul>'),
+					// move all list items after the first to this new popup ul
+					firstItemOffset = $firstItem.offset().top;
+				for (i = numItems; i = 1; i--) {
+					// Find all of the list items that have been pushed below the first item. Put those items into the popup menu. Put one additional item into the popup menu to cover situations where the last item is shorter than the "more" text.
+					$lastChild = $this.find('li:last-child');
+					keepLooking = (needsMenu($lastChild));
+					$lastChild.appendTo($popup);
+					if (!keepLooking) {
+						break;
+					}
+				}
+				// Our popup menu is currently in reverse order. Let's fix that.
+				$popup.children().each(function (i, li) {$popup.prepend(li); });
+				$this.append('<li class="flexMenu-viewMore"><a href="#" title="' + s.linkTitle + '">' + s.linkText + '</a></li>');
+				$moreItem = $this.find('li.flexMenu-viewMore');
+				$moreItem.append($popup);
+				$moreLink = $this.find('li.flexMenu-viewMore > a');
+				$moreLink.click(function (e) {
+					$popup.toggle();
+					$(this).parent().toggleClass('active'); // Using 'this' because toggling the class of $moreItem would sometimes toggle the wrong more link's class, or not toggle at all.
+					e.preventDefault();
+				});
+				if (s.showOnHover && (typeof Modernizr !== 'undefined') && !Modernizr.touch) { // If requireClick is  false AND touch is unsupported, then show the menu on hover. If Modernizr is not available, assume that touch is unsupported. Through the magic of lazy evaluation, we can check for Modernizr and start using it in the same if statement. Reversing the order of these variables would produce an error.
+					$moreItem.hover(
+						function () {
+							$popup.show();
+							$(this).addClass('active');
+						},
+						function () {
+							$popup.hide();
+							$(this).removeClass('active');
+						}
+					);
+				}
+			} else if (s.undo && $this.find('ul.flexMenu-popup')) {
+				$menu = $this.find('ul.flexMenu-popup');
+				numToRemove = $menu.find('li').length;
+				for (i = 1; i <= numToRemove; i++) {
+					$menu.find('li:first-child').appendTo($this);
+				}
+				$menu.remove();
+				$this.find('li.flexMenu-viewMore').remove();
+			}
+		});
+	};
+})(jQuery);
