cc5d386940950f1d5b80e47b6c8a50131d7a2ede
Blake McLeod <lbmcleod@gmail.com>
2013-02-05 00:53:27 -0500

    Fix for options lost on resize
    flexMenu objects now have their options property exposed. This allows
    the options to be re-applied on window resize.

--- flexmenu.js
+++ flexmenu.js
@@ -1,14 +1,18 @@
 /*	jQuery.flexMenu 1.1
 	https://github.com/352Media/flexMenu
-	Description: If a list is too long for all items to fit on one line, display a popup menu instead. 
+	Description: If a list is too long for all items to fit on one line, display a popup menu instead.
 	Dependencies: jQuery, Modernizr (optional). Without Modernizr, the menu can only be shown on click (not hover). */
 
 (function ($) {
-	var $flexParents = $(''), // A collection of all elements on which flexMenu has been called.
+	var flexObjects = [], // Array of all flexMenu objects
 		resizeTimeout;
 	// When the page is resized, adjust the flexMenus.
 	function adjustFlexMenu() {
-		$flexParents.flexMenu({'undo': true }).flexMenu();
+		$(flexObjects).each(function () {
+			$(this).flexMenu({
+				'undo' : true
+			}).flexMenu(this.options);
+		});
 	}
 	function collapseAllExcept($menuToAvoid) {
 		var $activeMenus,
@@ -19,20 +23,30 @@
 	}
 	$(window).resize(function () {
 		clearTimeout(resizeTimeout);
-	    resizeTimeout = setTimeout(adjustFlexMenu, 200);
+		resizeTimeout = setTimeout(function () {
+			adjustFlexMenu();
+		}, 200);
 	});
 	$.fn.flexMenu = function (options) {
-		var s = $.extend({
-			'threshold'	: 2,					// [integer] If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
-			'cutoff' : 2,						// [integer] If there is space for this many or fewer items outside our "more" popup, just move everything into the more menu. In that case, also use linkTextAll and linkTitleAll instead of linkText and linkTitle. To disable this feature, just set this value to 0.
-			'linkText' : 'More',				// [string] What text should we display on the "view more" link?
-			'linkTitle' : 'View More',			// [string] What should the title of the "view more" button be?
-			'linkTextAll' : 'Menu',				// [string] If we hit the cutoff, what text should we display on the "view more" link?
-			'linkTitleAll' : 'Open/Close Menu',	// [string] If we hit the cutoff, what should the title of the "view more" button be?
-			'showOnHover' : true,				// [boolean] Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
-			'popupAbsolute' : true,				// [boolean] Should we absolutely position the popup? Usually this is a good idea. That way, the popup can appear over other content and spill outside a parent that has overflow: hidden set. If you want to do something different from this in CSS, just set this option to false.
-			'undo' : false						// [boolean] Move the list items back to where they were before, and remove the "View More" link.
-		}, options);
+		var checkFlexObject,
+			s = $.extend({
+				'threshold' : 2, // [integer] If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
+				'cutoff' : 2, // [integer] If there is space for this many or fewer items outside our "more" popup, just move everything into the more menu. In that case, also use linkTextAll and linkTitleAll instead of linkText and linkTitle. To disable this feature, just set this value to 0.
+				'linkText' : 'More', // [string] What text should we display on the "view more" link?
+				'linkTitle' : 'View More', // [string] What should the title of the "view more" button be?
+				'linkTextAll' : 'Menu', // [string] If we hit the cutoff, what text should we display on the "view more" link?
+				'linkTitleAll' : 'Open/Close Menu', // [string] If we hit the cutoff, what should the title of the "view more" button be?
+				'showOnHover' : true, // [boolean] Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
+				'popupAbsolute' : true, // [boolean] Should we absolutely position the popup? Usually this is a good idea. That way, the popup can appear over other content and spill outside a parent that has overflow: hidden set. If you want to do something different from this in CSS, just set this option to false.
+				'undo' : false // [boolean] Move the list items back to where they were before, and remove the "View More" link.
+			}, options);
+		this.options = s; // Set options on object
+		checkFlexObject = $.inArray(this, flexObjects); // Checks if this object is already in the flexObjects array
+		if (checkFlexObject >= 0) {
+			flexObjects.splice(checkFlexObject, 1); // Remove this object if found
+		} else {
+			flexObjects.push(this); // Add this object to the flexObjects array
+		}
 		return this.each(function () {
 			var $this = $(this),
 				$self = $this,
@@ -54,10 +68,9 @@
 				// Values may be calculated from em and give us something other than round numbers. Browsers may round these inconsistently. So, let's round numbers to make it easier to trigger flexMenu.
 				return result;
 			}
-			$flexParents = $flexParents.add($this);
 			if (needsMenu($lastItem) && numItems > s.threshold && !s.undo && $this.is(':visible')) {
 				var $popup = $('<ul class="flexMenu-popup" style="display:none;' + ((s.popupAbsolute) ? ' position: absolute;' : '') + '"></ul>'),
-					// Move all list items after the first to this new popup ul
+				// Move all list items after the first to this new popup ul
 					firstItemOffset = $firstItem.offset().top;
 				for (i = numItems; i > 1; i--) {
 					// Find all of the list items that have been pushed below the first item. Put those items into the popup menu. Put one additional item into the popup menu to cover situations where the last item is shorter than the "more" text.
@@ -70,7 +83,6 @@
 						allInPopup = true;
 						break;
 					}
-
 					if (!keepLooking) {
 						break;
 					}
@@ -86,7 +98,9 @@
 					$this.find('li:nth-last-child(2)').appendTo($popup);
 				}
 				// Our popup menu is currently in reverse order. Let's fix that.
-				$popup.children().each(function (i, li) {$popup.prepend(li); });
+				$popup.children().each(function (i, li) {
+					$popup.prepend(li);
+				});
 				$moreItem.append($popup);
 				$moreLink = $this.find('li.flexMenu-viewMore > a');
 				$moreLink.click(function (e) {
@@ -106,8 +120,7 @@
 						function () {
 							$popup.hide();
 							$(this).removeClass('active');
-						}
-					);
+						});
 				}
 			} else if (s.undo && $this.find('ul.flexMenu-popup')) {
 				$menu = $this.find('ul.flexMenu-popup');
